bf668c9613f047b5721a7737fc1b73f04e30b844
nephila <nephila@nephila-pc>
2017-08-02 10:54:12 +0800

    Update 08/02/2018 -S update progress bar

--- resources/views/guidance/admission/studentinfo.blade.php
+++ resources/views/guidance/admission/studentinfo.blade.php
@@ -89,6 +89,11 @@
         @else
         <div class='alert alert-danger'>No Department yet has been set to the Applicant. Please see administrator.</div>
         @endif
+        
+        <?php
+        $status = \App\Status::where('idno', $list->idno)->first();
+        ?>
+        
         @if ($value==1)
         <h3>Entrance Exam</h3>
         <table class='table'>
@@ -101,89 +106,17 @@
                 <td>{{$exam->exam_result}}</td>
             </tr>
         </table> 
-        <?php
-        $status = \App\Status::where('idno', $list->idno)->first();
-        ?>
         @if ($status->status==0)
         <a href="{{url('guidance',array('viewmodifyinfo',$list->idno))}}"><div class='btn btn-primary col-sm-6'>Modify</div></a> 
         <a href="{{url('guidance',array('admission_slip',$list->idno))}}" target="_blank"><div class='btn btn-success col-sm-6'>Print Entrance Exam Slip</div></a>
 
-        <div class="row">
-            <div class="col-sm-12">
-                <ul class="nav nav-pills nav-justified thumbnail setup-panel">
-                    <li class="active"><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Guidance</h4>
-                            <p class="list-group-item-text">Take Entrance Exam</p>
-                        </a></li>
-                    <li class=""><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Dean's Office</h4>
-                            <p class="list-group-item-text">Go to Dean's Office for Assessment of Subjects</p>
-                        </a></li>
-                    <li class=""><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Cashier</h4>
-                            <p class="list-group-item-text">Go to Cashier for Payment</p>
-                        </a></li>
-                    <li class="e"><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Registrar's Office</h4>
-                            <p class="list-group-item-text">Go to Registrar's Office for Sectioning</p>
-                        </a></li>
-                </ul>
-            </div>
-        </div>
-
         @elseif ($status->status==1)
         <a href="{{url('guidance',array('viewmodifyinfo',$list->idno))}}"><div class='btn btn-primary col-sm-12'>Modify</div></a>
-
-        <div class="row">
-            <div class="col-sm-12">
-                <ul class="nav nav-pills nav-justified thumbnail setup-panel">
-                    <li class=""><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Guidance</h4>
-                            <p class="list-group-item-text">Take Entrance Exam</p>
-                        </a></li>
-                    <li class="active"><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Dean's Office</h4>
-                            <p class="list-group-item-text">Go to Dean's Office</p>
-                        </a></li>
-                    <li class=""><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Cashier</h4>
-                            <p class="list-group-item-text">Go to Cashier for Payment</p>
-                        </a></li>
-                    <li class=""><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Registrar's Office</h4>
-                            <p class="list-group-item-text">Go to Registrar's Office</p>
-                        </a></li>
-                </ul>
-            </div>
-        </div>
 
         @elseif ($status->status==-1)
         <div class="alert alert-danger">Sorry you have failed the Entrance Exam!!!</div>
         <a href="{{url('guidance',array('viewmodifyinfo',$list->idno))}}"><div class='btn btn-primary col-sm-12'>Modify</div></a>
         @elseif ($status->status==2)
-
-        <div class="row">
-            <div class="col-sm-12">
-                <ul class="nav nav-pills nav-justified thumbnail setup-panel">
-                    <li class=""><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Guidance</h4>
-                            <p class="list-group-item-text">Take Entrance Exam</p>
-                        </a></li>
-                    <li class=""><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Dean's Office</h4>
-                            <p class="list-group-item-text">Go to Dean's Office</p>
-                        </a></li>
-                    <li class="active"><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Cashier</h4>
-                            <p class="list-group-item-text">Go to Cashier for Payment</p>
-                        </a></li>
-                    <li class=""><a href="javascript:void(0)">
-                            <h4 class="list-group-item-heading">Registrar's Office</h4>
-                            <p class="list-group-item-text">Go to Registrar's Office</p>
-                        </a></li>
-                </ul>
-            </div>
-        </div>
 
         @endif
         @else
@@ -191,32 +124,32 @@
         <a href="{{url('guidance',array('viewmodifyinfo',$list->idno))}}"><div class='btn btn-primary col-sm-6'>Modify</div></a> 
         <a href="{{url('guidance',array('schedule_applicant',$list->idno))}}"><div class='btn btn-success col-sm-6'>Schedule Entrance Exam</div></a>
         
+        @endif
+        
+        @if($status->status!==-1)
         <div class="row">
             <div class="col-sm-12">
                 <ul class="nav nav-pills nav-justified thumbnail setup-panel">
-                    <li class="active"><a href="javascript:void(0)">
+                    <li @if ($status->status==0) class="active" @endif><a href="javascript:void(0)">
                             <h4 class="list-group-item-heading">Guidance</h4>
                             <p class="list-group-item-text">Take Entrance Exam</p>
                         </a></li>
-                    <li class=""><a href="javascript:void(0)">
+                    <li @if ($status->status==1) class="active" @endif><a href="javascript:void(0)">
                             <h4 class="list-group-item-heading">Dean's Office</h4>
                             <p class="list-group-item-text">Go to Dean's Office</p>
                         </a></li>
-                    <li class=""><a href="javascript:void(0)">
+                    <li @if ($status->status==2) class="active" @endif><a href="javascript:void(0)">
                             <h4 class="list-group-item-heading">Cashier</h4>
                             <p class="list-group-item-text">Go to Cashier for Payment</p>
                         </a></li>
-                    <li class="e"><a href="javascript:void(0)">
+                    <li @if ($status->status==3) class="active" @endif><a href="javascript:void(0)">
                             <h4 class="list-group-item-heading">Registrar's Office</h4>
                             <p class="list-group-item-text">Go to Registrar's Office</p>
                         </a></li>
                 </ul>
             </div>
         </div>
-        
         @endif
-
-        </form>
     </div>
 </div>
 @stop
