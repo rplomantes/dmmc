ae5380a9d7f38a08dd2a8a5ab51d53c79725bcae
nephila <nephila@nephila-pc>
2017-08-18 16:47:42 +0800

    Update 08/17/2018 -S update

---
+++ app/Http/Controllers/Registrar/Assessment/AssessmentController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers\Registrar\Assessment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+class AssessmentController extends Controller {
+    function indexcollege(){
+        return view('registrar.assessment.indexcollege');
+    }
+    
+    function indexshs(){
+        return view('registrar.assessment.indexshs');
+    }
+    
+    function viewinfo($idno) {
+        $status = \App\Status::where('idno', $idno)->first();
+        return view('registrar.assessment.studentinfo', compact('status', 'idno'));
+    }
+    function viewassessment($idno) {
+        $status = \App\Status::where('idno', $idno)->first();
+        return view('registrar.assessment.viewassessment', compact('status', 'idno'));
+    }
+}
